afa1b23d6595caca796e9028a3b6875151b061c9
Warreign <warreignn@gmail.com>
2024-04-04 18:10:47 -0700

    Add mouse move event, finish other mouse event constructors and data members

--- GraphicalEngine/src/events/MouseEvent.h
+++ GraphicalEngine/src/events/MouseEvent.h
@@ -1,21 +1,45 @@
 #pragma once
 
 #include "events/Event.h"
+#include "MouseKeys.h"
 
 namespace Warreign
 {
-	class MousePressEvent : public Event
+	class MouseButtonEvent : public Event
 	{
 	public:
-		MousePressEvent() = default;
+		MouseButtonEvent(const MouseButton button)
+			: m_button(button)
+		{
+		}
+
+		MouseButton getButton() const
+		{
+			return m_button;
+		}
+
+	private:
+		MouseButton m_button;
+	};
+
+	class MousePressEvent : public MouseButtonEvent
+	{
+	public:
+		MousePressEvent(const MouseButton button)
+			: MouseButtonEvent(button)
+		{
+		}
 
 		EVENT_TYPE(MousePress)
 	};
 
-	class MouseReleaseEvent : public Event
+	class MouseReleaseEvent : public MouseButtonEvent
 	{
 	public:
-		MouseReleaseEvent() = default;
+		MouseReleaseEvent(const MouseButton button)
+			: MouseButtonEvent(button)
+		{
+		}
 
 		EVENT_TYPE(MouseRelease)
 	};
@@ -23,8 +47,47 @@ namespace Warreign
 	class MouseScrollEvent : public Event
 	{
 	public:
-		MouseScrollEvent() = default;
+		MouseScrollEvent(const float xOffset, const float yOffset)
+			: m_xOffset(xOffset), m_yOffset(yOffset)
+		{
+		}
+
+		float getXOffset() const
+		{
+			return m_xOffset;
+		}
+
+		float getYOffset() const
+		{
+			return m_yOffset;
+		}
+
+	private:
+		const float m_xOffset;
+		const float m_yOffset;
 
 		EVENT_TYPE(MouseScroll)
+	};
+
+	class MouseMoveEvent : public Event
+	{
+	public:
+		MouseMoveEvent(const float x, const float y)
+			: m_x(x), m_y(y)
+		{
+		}
+
+		float getX() const 
+		{
+			return m_x;
+		}
+
+		float getY() const 
+		{
+			return m_y;
+		}
+
+	private:
+		const float m_x, m_y;
 	};
 }
